7c712d79a8581c62659baf06866c598b2514d310
m0rt <149887283+nodem0rt@users.noreply.github.com>
2024-11-25 16:27:31 +0700

    Update multiplenode.sh

--- multiplenode.sh
+++ multiplenode.sh
@@ -1,2 +1,2 @@
-wget -O multiplenode.sh <script-URL> && chmod +x <output-filename> && ./<output-filename>
+wget -O multiplenode.sh https://raw.githubusercontent.com/nodem0rt/Multiple/refs/heads/main/multiplenode.sh && chmod +x multiplenode.sh && ./multiplenode.sh
 
