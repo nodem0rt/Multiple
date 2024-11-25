bf0ba209127d9e3191b84236af9a0dc1066a6b6c
m0rt <149887283+nodem0rt@users.noreply.github.com>
2024-11-25 17:08:46 +0700

    Update multiplenode.sh

--- multiplenode.sh
+++ multiplenode.sh
@@ -1,22 +1,15 @@
 #!/bin/bash
 
-# Auto-Install Script for Multiple for Linux
-# Tested for Ubuntu
-
-# Variables
-URL="https://cdn.app.multiple.cc/client/linux/x64/multipleforlinux.tar"
-
 # Ensure the script is run as root
 if [ "$(id -u)" -ne 0 ]; then
     echo "This script must be run as root. Use sudo to run it."
     exit 1
 fi
 
-# Step 1: Update & Download the tar file
+echo "Starting system update..."
 sudo apt update && sudo apt upgrade -y
 
 echo "Checking system architecture..."
-
 ARCH=$(uname -m)
 if [[ "$ARCH" == "x86_64" ]]; then
     CLIENT_URL="https://cdn.app.multiple.cc/client/linux/x64/multipleforlinux.tar"
@@ -33,14 +26,12 @@ wget $CLIENT_URL -O multipleforlinux.tar
 echo "Extracting files..."
 tar -xvf multipleforlinux.tar
 
-# Step 2: Extract the tar file
 cd multipleforlinux
 
 echo "Granting permissions..."
 chmod +x ./multiple-cli
 chmod +x ./multiple-node
 
-# Step 3: Add to PATH (Optional)
 echo "Adding directory to system PATH..."
 echo "PATH=\$PATH:$(pwd)" >> ~/.bash_profile
 source ~/.bash_profile
@@ -51,12 +42,12 @@ chmod -R 777 $(pwd)
 echo "Launching multiple-node..."
 nohup ./multiple-node > output.log 2>&1 &
 
-echo "Please enter your Identification Code and Create New PIN to bind your account:"
-read -p "Identification Code: " IDENTIFICATION CODE
+echo "Please enter your Identification Code and create PIN to bind your account:"
+read -p "Identification Code: " IDENTIFICATION
 read -p "Set your PIN: " PIN
 
-echo "Binding account with ID: $IDENTIFICATIONCODE and PIN: $PIN..."
-multiple-cli bind --bandwidth-download 100 --identifier $IDENTIFICATIONCODE --pin $PIN --storage 200 --bandwidth-upload 100
+echo "Binding account with ID: $IDENTIFICATION and PIN: $PIN..."
+multiple-cli bind --bandwidth-download 100 --identifier $IDENTIFICATION --pin $PIN --storage 200 --bandwidth-upload 100
 
-# Final message
-echo "Installation complete! Don't Forget to Join with Us : https://t.me/CryptoNodeID."
+echo "Installation complete!
+echo  "Come Join with Us : https://t.me/CryptoNodeID"
